53c68e94c8dc7f0dc3869ea9fed86a3c5fac7a83
webok21 <webok21@gmail.com>
2021-06-01 18:49:06 +0200

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,31 +16,30 @@ const SecTimeout = () => {
 SecTimeout()
 
 
-
 // 2.)
 
-// let timeInterval = setInterval(() => {
-//     let now = new Date();
-//     let hours = now.getHours();
-//     let minutes = now.getMinutes();
-//     let seconds = now.getSeconds();
-//     console.log(hours + ":" + minutes + ":" + seconds)
-// }, 1000)
+let timeInterval = setInterval(() => {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    console.log(hours + ":" + minutes + ":" + seconds)
+}, 1000)
 
 
-// 3.) 
+// 3.)
 
-// let timeCounter = 11
+let timeCounter = 11
 
-// let counter = setInterval(() => {
-//     timeCounter--
-//     if (timeCounter >= 1) {
-//         console.log(timeCounter)
-//     } else {
-//         console.log("Endlich Feierabend")
-//         clearInterval(counter)
-//     }
-// }, 1000)
+let counter = setInterval(() => {
+    timeCounter--
+    if (timeCounter >= 1) {
+        console.log(timeCounter)
+    } else {
+        console.log("Endlich Feierabend")
+        clearInterval(counter)
+    }
+}, 1000)
 
 
 
